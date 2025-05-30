e3c6f7e20cabca5be1f8ae7a9021f33e9906fac8
Himanshu2557-coder <himanshusainidtu@gmail.com>
2025-05-30 11:31:20 +0530

    commit

---
+++ INTRO/hello.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int num;
+    cout << "Enter a number: ";
+    cin >> num;
+
+    int square = num * num;
+
+    cout << "Square of " << num << " is " << square << endl;
+    return 0;
+}
